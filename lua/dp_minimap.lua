e30206101860b41e31dae241a9de874385985282
peter-lyr <llydrp.ldp@gmail.com>
2024-05-05 22:34:16 +0800

    git@github.com:peter-lyr/dp_minimap

---
+++ lua/dp_minimap.lua
@@ -0,0 +1,23 @@
+local minimap = require 'mini.map'
+local symbols = minimap.gen_encode_symbols.dot '4x2'
+
+symbols[1] = ' '
+
+minimap.setup {
+  integrations = {
+    minimap.gen_integration.builtin_search(),
+    minimap.gen_integration.gitsigns(),
+    minimap.gen_integration.diagnostic(),
+  },
+  symbols = {
+    encode = symbols,
+  },
+  window = {
+    focusable = true,
+    -- side = 'right',
+    -- show_integration_count = true,
+    -- width = 10,
+    -- winblend = 25,
+    -- zindex = 10,
+  },
+}
